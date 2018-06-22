1babab3c0b3f5d73b83d3817b34d21d8701e37c8
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-06-22 14:52:47 +0200

    Add preliminary bounding structure

---
+++ include/termlike/bounds.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdint.h> // int32_t
+
+enum term_wrap {
+    TERM_WRAP_CHARACTERS,
+    TERM_WRAP_WORDS
+};
+
+struct term_bounds {
+    enum term_wrap wrap;
+    int32_t width;
+    int32_t height;
+};
+
+extern struct term_bounds const TERM_BOUNDS_NONE;
+
+struct term_bounds bounded(int32_t width, int32_t height);
+struct term_bounds boundedw(int32_t width, int32_t height, enum term_wrap);
---
+++ src/bounds.c
@@ -0,0 +1,23 @@
+#include <termlike/bounds.h> // term_bounds
+
+struct term_bounds const TERM_BOUNDS_NONE = {
+    .wrap = TERM_WRAP_CHARACTERS,
+    .width = 0,
+    .height = 0
+};
+
+struct term_bounds
+bounded(int32_t const width, int32_t const height)
+{
+    return boundedw(width, height, TERM_WRAP_WORDS);
+}
+
+struct term_bounds
+boundedw(int32_t const width, int32_t const height, enum term_wrap const wrap)
+{
+    return (struct term_bounds) {
+        .wrap = wrap,
+        .width = width,
+        .height = height
+    };
+}

8d3518740fd9e42bb19b87dd1d835013e05b32fd
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-08-11 11:42:25 +0200

    Inline make functions

--- include/termlike/position.h
+++ include/termlike/position.h
@@ -14,8 +14,28 @@ struct term_position {
     struct term_layer layer;
 };
 
-struct term_position positioned(int32_t x, int32_t y);
-struct term_position positionedz(int32_t x, int32_t y, struct term_layer);
+inline
+struct term_position
+positionedz(int32_t const x,
+            int32_t const y,
+            struct term_layer const layer)
+{
+    return (struct term_position) {
+        .location = {
+            .x = x,
+            .y = y
+        },
+        .layer = layer
+    };
+}
+    
+inline
+struct term_position
+positioned(int32_t const x,
+           int32_t const y)
+{
+    return positionedz(x, y, TERM_LAYER_DEFAULT);
+}
 
 struct term_position positioned_offset(struct term_position,
                                        int32_t dx, int32_t dy);
--- src/position.c
+++ src/position.c
@@ -1,28 +1,8 @@
 #include <termlike/position.h> // term_position, positioned*
 
-struct term_position
-positioned(int32_t const x, int32_t const y)
-{
-    return (struct term_position) {
-        .location = {
-            .x = x,
-            .y = y
-        },
-        .layer = TERM_LAYER_DEFAULT
-    };
-}
-
-struct term_position
-positionedz(int32_t const x, int32_t const y, struct term_layer layer)
-{
-    return (struct term_position) {
-        .location = {
-            .x = x,
-            .y = y
-        },
-        .layer = layer
-    };
-}
+extern inline struct term_position positioned(int32_t x, int32_t y);
+extern inline struct term_position positionedz(int32_t x, int32_t y,
+                                               struct term_layer);
 
 struct term_position
 positioned_offset(struct term_position position,

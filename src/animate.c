d9f815cf489c6873ec57113439be5b507ed5328c
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2019-02-21 14:53:04 +0100

    Refactor and remove unnecessary functions

--- src/animate.c
+++ src/animate.c
@@ -8,11 +8,6 @@
 extern inline struct term_animate animated(float value);
 
 static void animate_set(struct term_animate *, float value);
-
-static float animate_blend_value(float previous, float next, double interp);
-static float animate_from_to(float a, float b, double time, double duration);
-
-static bool animate_equals(float a, float b);
 static float animate_lerp(float a, float b, float t);
 
 void
@@ -23,7 +18,7 @@ animate_to(struct term_animate * const animatable,
 {
     animatable->time += step;
 
-    if (duration > SECONDS(0)) {
+    if (duration > SECONDS(0) && animatable->time < duration) {
         double t = animatable->time;
 
         if (t < SECONDS(0)) {
@@ -32,10 +27,11 @@ animate_to(struct term_animate * const animatable,
             t = duration;
         }
 
-        animate_set(animatable, animate_from_to(animatable->value.original,
-                                                value,
-                                                t,
-                                                duration));
+        float const scalar = (float)(t / duration);
+
+        animate_set(animatable, animate_lerp(animatable->value.original,
+                                             value,
+                                             scalar));
     } else {
         animate_set(animatable, value);
     }
@@ -56,9 +52,9 @@ animate_blendf(struct term_animate const animatable,
                double const interpolation,
                float * const value)
 {
-    *value = animate_blend_value(animatable.value.previous,
-                                 animatable.value.current,
-                                 interpolation);
+    *value = animate_lerp(animatable.value.previous,
+                          animatable.value.current,
+                          interpolation);
 }
 
 void
@@ -79,55 +75,6 @@ animate_set(struct term_animate * const animatable, float const value)
 {
     animatable->value.previous = animatable->value.current;
     animatable->value.current = value;
-}
-
-static
-float
-animate_from_to(float const a,
-                float const b,
-                double const time,
-                double const duration)
-{
-    float const scalar = duration > SECONDS(0) ?
-        (float)(time / duration) : 1;
-
-    float d = animate_lerp(a, b, scalar);
-
-    if (animate_equals(d, b)) {
-        d = b;
-    }
-
-    return d;
-}
-
-static
-float
-animate_blend_value(float const previous,
-                    float const next,
-                    double const interpolation)
-{
-    if (animate_equals(previous, next)) {
-        return next;
-    }
-
-    float scalar = (float)interpolation;
-
-    if (scalar < 0) {
-        scalar = 0;
-    } else if (scalar > 1) {
-        scalar = 1;
-    }
-
-    return animate_lerp(previous, next, scalar);
-}
-
-static
-bool
-animate_equals(float const a, float const b)
-{
-    static double const precision = 1.0 / 1048;
-
-    return fabsf(b - a) < precision;
 }
 
 static
